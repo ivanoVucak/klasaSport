ddc890340a34a2aa9a33f507a1894cc7e5f9bfd7
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2024-01-23 10:49:16 +0100

    Add files via upload

---
+++ klasaSport/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaSport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sport sport = new sport("Nogoomet", "Da", 11);
+            sport sport2 = new sport("Vaterpolo", "Da", 7);
+            sport sport3 = new sport("Curling", "Ne", 1);
+
+            Console.WriteLine(sport.ToString());
+            Console.WriteLine(sport2.ToString());
+            Console.WriteLine(sport3.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ klasaSport/sport.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaSport
+{
+    internal class sport
+    {
+        string naziv, igraLiSeLoptom;
+        int brojIgraca;
+
+        public void setNaziv(string naziv) 
+        {
+            this.naziv = naziv;
+        }
+        public string getNaziv()
+        {
+            return this.naziv;
+        }
+        public void setIgraLiSeLoptom(string igraLiSeLoptom)
+        {
+            this.igraLiSeLoptom = igraLiSeLoptom;
+        }
+        public string getIgraLiSeLoptom()
+        {
+            return this.igraLiSeLoptom;
+        }
+        public void setBrojIgraca(int brojIgraca)
+        {
+            this.brojIgraca = brojIgraca;
+        }
+        public int getBrojIgraca()
+        {
+            return this.brojIgraca;
+        }
+
+        public override string ToString()
+        {
+            string ispis = "Naziv --> " + this.getNaziv() + "\n" + "Igra li se loptom --> " + this.getIgraLiSeLoptom()
+            + "\n" + "Broj igraca --> " + this.getBrojIgraca() + "\n";
+            return ispis;
+        }
+        public sport(string naziv, string igraLiSeLoptom, int brojIgraca) {
+            this.naziv = naziv; this.igraLiSeLoptom = igraLiSeLoptom; this.brojIgraca = brojIgraca;
+        }
+    }
+}
